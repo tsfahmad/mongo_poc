a8933dcb467cd60df014a6f696dff2bc88d1c92f
Tauseef Ahmad <tsfahmad360@gmail.com>
2021-07-02 17:43:37 +0530

    Mongo create product

--- app.js
+++ app.js
@@ -1,11 +1,17 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+require('dotenv').config()
+
+
+
+const { createProduct } = require('./mongo');
 
 const app = express();
 
+console.log(process.env.MONGO_URL);
 app.use(bodyParser.json());
 
-app.post('/products');
+app.post('/products', createProduct);
 
 app.get('/products');
 
---
+++ mongo.js
@@ -0,0 +1,40 @@
+const MongoClient = require('mongodb').MongoClient;
+
+
+const createProduct = async (req, res, next) => {
+    const newProduct = {
+        name: req.body.name,
+        price: req.body.price,
+    }
+
+    console.log(process.env.MONGO_URL);
+    const client = MongoClient(
+        process.env.MONGO_URL,
+        {useNewUrlParser: true, useUnifiedTopology: true}
+    );
+
+    try {
+        await client.connect();
+        const db = client.db();
+        const result = await db.collection('products').insertOne(newProduct);
+    }
+    catch (error) {
+        console.log(error);
+        return res.json({message: 'Could not store data.'});
+    }
+    finally {
+        client.close();
+    }
+
+    res.json(newProduct);
+};
+
+const getProducts = async (req, res, next) => {
+
+};
+
+
+module.exports = {
+    createProduct,
+    getProducts
+}
